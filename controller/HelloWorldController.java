3ab3f886ba30de4e367fd7292916e0ac3a035cec
Ana-Gatti <anagatti@live.com>
2022-03-23 22:18:09 -0300

    meu primeiro Spring

---
+++ controller/HelloWorldController.java
@@ -0,0 +1,34 @@
+package com.generation.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/hello-world")
+public class HelloWorldController {
+	
+	@GetMapping
+	public String helloworld() {
+		return "Hello World!!";	
+	}
+	
+	@GetMapping ("/bsm")
+	public String bsmgeneration() {
+		return "Mentalidade de Crescimento\r\n"
+				+ "Persistência\r\n"
+				+ "Responsabilidade Pessoal\r\n"
+				+ "Orientação ao Futuro\r\n"
+				+ "Comunicação\r\n"
+				+ "Orientação ao Detalhe\r\n"
+				+ "Proatividade\r\n"
+				+ "Trabalho em Equipe";	
+	}
+	
+	@GetMapping ("/bsm/objetivo")
+	public String objetivos() {
+		return "Aprender Spring e todo conteúdo da semana ";	
+	}
+	
+}
+
